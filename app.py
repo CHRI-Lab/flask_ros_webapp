01a56ac936034ab0407f6087017e2c8ced5f4b78
Wafa Johal <wafa.johal@gmail.com>
2023-07-04 13:43:11 +1000

    clean version with refresh

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ from flask import Flask, render_template, request # Flask
 
 # ROS imports to setup the node
 import rclpy
-from rclpy.node import Node
 from std_msgs.msg import String
 
 # Imports for threading operations
-import signal, sys
-from threading import Thread, Event
+import sys
+from threading import Thread
+import atexit
 
 new_image = None # Global variable to hold the image name
 
-event = Event()
+
 
 ##### Setting up the ROS node:
 def listener_callback(msg):
     global new_image
     node.get_logger().info('I heard: "%s"' % msg.data)
     new_image = msg.data
-    event.set()
+
 
 
 # Initializing the node
@@ -34,8 +34,6 @@ app = Flask(__name__)
 
 def get_image():
     rclpy.spin_once(node,timeout_sec=1.0)
-    event.wait()
-    event.clear()
     return new_image
 
 @app.route('/')
@@ -49,15 +47,17 @@ def change_image():
     return render_template('index.html', new_image=new_image)
 
 
-## Function that finish the actual context
-def signal_handler(signal, new_image):
+#defining function to run on shutdown
+def close_running_threads():
     rclpy.shutdown()
+    print("closed ROS")
     sys.exit(0)
+    
 
-signal.signal(signal.SIGINT,signal_handler) # Calls the 'signal_handler' and finish the actual signal (like a Ctrl + C)
- 
 ## Main funcion, only initiate the Flask app
 def main(args=None):
+    atexit.register(close_running_threads)
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = -1
     app.run(host='0.0.0.0', port=5000 ,debug=False)
 
 
